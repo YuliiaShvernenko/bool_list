e983c1c1c3dc3406f9c79141c659c7694ce10126
YuliiaShvernenko <julija1072007@gmail.com>
2025-12-17 16:56:28 +0200

    did first task

---
+++ bool_list/first_task.py
@@ -0,0 +1,14 @@
+first_number = int(input('Enter the first number:'))
+second_number = int(input('Enter the second number:'))
+operation = input('Enter the operation (+, -, *, /):')
+
+if operation == '+':
+    print(first_number + second_number)
+elif operation == '-':
+    print(first_number - second_number)
+elif operation == '*':
+    print(first_number * second_number)
+elif operation == '/' and second_number == 0:
+    print('Division by zero is impossible')
+else:
+    print(first_number / second_number)
